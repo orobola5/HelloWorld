aa749a340ae44d3aa6a6e67e13267261059371ba
Love Olanrewaju <olanrewajulove2@gmail.com>
2022-10-05 22:34:02 +0100

    initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,22 @@
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
+
+    var language="french"
+    val score=98
+
+    var numOne = 20
+    var numTwo = 10
+
+    println("$numOne + $numTwo = ${numOne+numTwo}")
+    println("$numOne - $numTwo = ${numOne-numTwo}")
+    println("$numOne * $numTwo = ${numOne*numTwo}")
+    println("$numOne / $numTwo = ${numOne/numTwo}")
+    println("$numOne % $numTwo = ${numOne %numTwo}")
+
+
+
+}
